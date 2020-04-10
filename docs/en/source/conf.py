53627f0d5d4c4eae7b03fb195cac4429a91d96f7
Wilhelm Putz <jinjamator@aci.guru>
2020-04-10 09:58:45 +0200

    add docx builder

--- docs/en/source/conf.py
+++ docs/en/source/conf.py
@@ -32,7 +32,8 @@ release = '0.3'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-'recommonmark'
+'recommonmark',
+'docxbuilder'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
