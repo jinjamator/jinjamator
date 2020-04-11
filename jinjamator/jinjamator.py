102661f38475f71037e5e89e5b237ccc4940b54c
Wilhelm Putz <jinjamator@aci.guru>
2020-04-11 16:37:46 +0200

    bump version

--- jinjamator/jinjamator.py
+++ jinjamator/jinjamator.py
@@ -27,7 +27,7 @@ from glob import glob
 import tempfile
 from jinjamator.plugin_loader.output import load_output_plugin
 
-__version__ = "0.4"
+__version__ = "0.4.1"
 __updated__ = "10.05.2020"
 __date__ = "22.07.2019"
 __author__ = "Wilhelm Putz"
