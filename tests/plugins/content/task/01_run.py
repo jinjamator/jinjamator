9a09b119916f78b663cc1f35c38ded1f7e1807c6
Wilhelm Putz <jinjamator@aci.guru>
2020-04-19 09:05:08 +0200

    hide subtask

--- tests/plugins/content/task/01_run.py
+++ tests/plugins/content/task/01_run.py
@@ -1,5 +1,5 @@
 ok = True
-for subtask_path, return_value in task.run("subtask1/", output_plugin="null").items():
+for subtask_path, return_value in task.run(".subtask1/", output_plugin="null").items():
     if "01_tasklet_1" not in subtask_path:
         ok = False
     if return_value != "OK":
