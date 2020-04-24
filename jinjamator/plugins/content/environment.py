53cc509c7ff24b71d01327b61d71f1feec58c227
Wilhelm Putz <jinjamator@aci.guru>
2020-04-24 21:22:02 +0200

    cleanup

--- jinjamator/plugins/content/environment.py
+++ jinjamator/plugins/content/environment.py
@@ -1,6 +1,9 @@
-import logging 
+import logging
+
 log = logging.getLogger()
+
+
 def site_path():
     log.debug(self._parent._configuration._data)
-    
-    return self._parent._configuration.get('jinjamator_site_path',None)
+
+    return self._parent._configuration.get("jinjamator_site_path", None)
