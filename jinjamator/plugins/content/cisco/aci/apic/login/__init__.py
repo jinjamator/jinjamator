601acaf79f1f0b9be89edf8b5f9ba8bdca1d0b08
Wilhelm Putz <jinjamator@aci.guru>
2020-04-27 10:20:15 +0200

    actually test login

--- jinjamator/plugins/content/cisco/aci/apic/login/__init__.py
+++ jinjamator/plugins/content/cisco/aci/apic/login/__init__.py
@@ -1,7 +1,9 @@
-def is_possible():
+def is_possible(timeout = 1):
     """ 
-    Check if all vars for apic login are defined.
+    Check if all vars for apic login are defined and make a test query to determine if the login works
     """
+    from jinjamator.plugin_loader.content import py_load_plugins
+    py_load_plugins(globals())
     if (
         "apic_url" in self._parent.configuration._data
         and "apic_username" in self._parent.configuration._data
@@ -10,5 +12,17 @@ def is_possible():
         "apic_key" in self._parent.configuration._data
         and "apic_cert_name" in self._parent.configuration._data
     ):
+        
+        
+        import logging
+        log = logging.getLogger()
+        
+        try:
+            sess = cisco.aci.connect_apic()
+            sess.get('/api/aaaLogin.json',1)
+        except Exception as e:
+            log.error(e)
+            return False
+        
         return True
     return False
