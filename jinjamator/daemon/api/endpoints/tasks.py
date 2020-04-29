5e54ecf20bf38eef0034b72cb2745b63b395667b
Wilhelm Putz <jinjamator@aci.guru>
2020-04-29 21:56:42 +0200

    fix duplicate registration

--- jinjamator/daemon/api/endpoints/tasks.py
+++ jinjamator/daemon/api/endpoints/tasks.py
@@ -58,10 +58,13 @@ def discover_tasks(app):
                     if dir_chunk.startswith(".") or dir_chunk in ["__pycache__"]:
                         append = False
                         break
-
-                if append:
-                    dir_name = task_dir.replace(tasks_base_dir, "")[1:]
+                
+                dir_name = task_dir.replace(tasks_base_dir, "")[1:]
+                if append and dir_name not in available_tasks_by_path:
+                    
                     task_id = xxhash.xxh64(task_dir).hexdigest()
+                    
+                    
                     task_info = {
                         "id": task_id,
                         "path": dir_name,
@@ -69,143 +72,142 @@ def discover_tasks(app):
                         "description": get_section_from_task_doc(task_dir)
                         or "no description",
                     }
-                    if task_id not in available_tasks_by_path:
-                        available_tasks_by_path[dir_name] = task_info
-                        try:
-                            task = JinjamatorTask()
-                            log.debug(app.config["JINJAMATOR_FULL_CONFIGURATION"])
-                            task._configuration.merge_dict(
-                                app.config["JINJAMATOR_FULL_CONFIGURATION"]
+                    available_tasks_by_path[dir_name] = task_info
+                    try:
+                        task = JinjamatorTask()
+                        log.debug(app.config["JINJAMATOR_FULL_CONFIGURATION"])
+                        task._configuration.merge_dict(
+                            app.config["JINJAMATOR_FULL_CONFIGURATION"]
+                        )
+
+                        task.load(
+                            os.path.join(task_info["base_dir"], task_info["path"])
+                        )
+                        with app.app_context():
+                            data = json.loads(
+                                jsonify(
+                                    task.get_jsonform_schema()["schema"]
+                                ).data.decode("utf-8")
                             )
+                        task_models[task_info["path"]] = api.schema_model(
+                            task_id, data
+                        )
+                        del task
 
-                            task.load(
-                                os.path.join(task_info["base_dir"], task_info["path"])
+                        log.info(f"registred model for task {task_dir}")
+
+                        @ns.route(
+                            f"/{task_info['path']}", endpoint=task_info["path"]
+                        )
+                        class APIJinjamatorTask(Resource):
+                            @api.doc(
+                                f"get_task_{task_info['path'].replace(os.path.sep,'_')}_schema"
                             )
-                            with app.app_context():
-                                data = json.loads(
-                                    jsonify(
-                                        task.get_jsonform_schema()["schema"]
-                                    ).data.decode("utf-8")
+                            @api.expect(task_arguments)
+                            def get(self):
+                                """
+                                Returns the json-schema or the whole alpacajs configuration data for the task
+                                """
+                                args = task_arguments.parse_args(request)
+                                schema_type = args.get("schema-type", "full")
+                                environment_site = args.get(
+                                    "preload-defaults-from-site"
                                 )
-                            task_models[task_info["path"]] = api.schema_model(
-                                task_id, data
-                            )
-                            del task
-
-                            log.info(f"registred model for task {task_dir}")
-
-                            @ns.route(
-                                f"/{task_info['path']}", endpoint=task_info["path"]
-                            )
-                            class APIJinjamatorTask(Resource):
-                                @api.doc(
-                                    f"get_task_{task_info['path'].replace(os.path.sep,'_')}_schema"
+                                relative_task_path = request.endpoint.replace(
+                                    "api.", ""
                                 )
-                                @api.expect(task_arguments)
-                                def get(self):
-                                    """
-                                    Returns the json-schema or the whole alpacajs configuration data for the task
-                                    """
-                                    args = task_arguments.parse_args(request)
-                                    schema_type = args.get("schema-type", "full")
-                                    environment_site = args.get(
-                                        "preload-defaults-from-site"
-                                    )
-                                    relative_task_path = request.endpoint.replace(
-                                        "api.", ""
-                                    )
-                                    inner_task = JinjamatorTask()
+                                inner_task = JinjamatorTask()
 
-                                    inner_task._configuration.merge_dict(
-                                        app.config["JINJAMATOR_FULL_CONFIGURATION"]
-                                    )
-                                    inner_task.load(relative_task_path)
-
-                                    if environment_site not in [None, "None"]:
-                                        inner_task._configuration[
-                                            "jinjamator_site_path"
-                                        ] = site_path_by_name.get(environment_site)
-                                        inner_task._configuration[
-                                            "jinjamator_site_name"
-                                        ] = environment_site
-                                        inner_task.configuration.merge_yaml(
-                                            "{}/defaults.yaml".format(
-                                                site_path_by_name.get(environment_site)
-                                            )
-                                        )
-
-                                    full_schema = inner_task.get_jsonform_schema()
-
-                                    if schema_type in ["", "full"]:
-                                        response = jsonify(full_schema)
-                                    elif schema_type in ["schema"]:
-                                        response = jsonify(
-                                            full_schema.get("schema", {})
-                                        )
-                                    elif schema_type in ["data"]:
-                                        response = jsonify(full_schema.get("data", {}))
-                                    elif schema_type in ["options"]:
-                                        response = jsonify(
-                                            full_schema.get("options", {})
-                                        )
-                                    elif schema_type in ["view"]:
-                                        response = jsonify(full_schema.get("view", {}))
-                                    del inner_task
-                                    return response
-
-                                @api.doc(
-                                    f"create_task_instance_for_{task_info['path'].replace(os.path.sep,'_')}"
+                                inner_task._configuration.merge_dict(
+                                    app.config["JINJAMATOR_FULL_CONFIGURATION"]
                                 )
-                                @api.expect(
-                                    task_models[task_info["path"]], validate=False
-                                )
-                                def post(self):
-                                    """
-                                    Creates an instance of the task and returns the job_id
-                                    """
+                                inner_task.load(relative_task_path)
 
-                                    from jinjamator.task.celery import (
-                                        run_jinjamator_task,
-                                    )
-                                    from jinjamator.daemon.database import db
-
-                                    relative_task_path = request.endpoint.replace(
-                                        "api.", ""
-                                    )
-                                    data = request.get_json()
-                                    job_id = str(uuid.uuid4())
-                                    job = run_jinjamator_task.apply_async(
-                                        [
-                                            relative_task_path,
-                                            data,
-                                            data.get("output_plugin", "console"),
-                                        ],
-                                        task_id=job_id,
-                                    )
-
-                                    db_job = list(
-                                        db.session.query(DB_Job).filter(
-                                            DB_Job.task_id == job.id
+                                if environment_site not in [None, "None"]:
+                                    inner_task._configuration[
+                                        "jinjamator_site_path"
+                                    ] = site_path_by_name.get(environment_site)
+                                    inner_task._configuration[
+                                        "jinjamator_site_name"
+                                    ] = environment_site
+                                    inner_task.configuration.merge_yaml(
+                                        "{}/defaults.yaml".format(
+                                            site_path_by_name.get(environment_site)
                                         )
                                     )
-                                    db_job = db_job and db_job[0]
-                                    if not db_job:
-                                        db_job = DB_Job(job.id)
-                                        db_job.status = "SCHEDULED"
-                                        db_job.configuration = data
-                                        db_job.jinjamator_task = relative_task_path
-                                        db.session.add(db_job)
-                                        db.session.flush()
-                                        db.session.commit()
 
-                                    return jsonify({"job_id": job.id})
+                                full_schema = inner_task.get_jsonform_schema()
 
-                        except Exception as e:
-                            import traceback
+                                if schema_type in ["", "full"]:
+                                    response = jsonify(full_schema)
+                                elif schema_type in ["schema"]:
+                                    response = jsonify(
+                                        full_schema.get("schema", {})
+                                    )
+                                elif schema_type in ["data"]:
+                                    response = jsonify(full_schema.get("data", {}))
+                                elif schema_type in ["options"]:
+                                    response = jsonify(
+                                        full_schema.get("options", {})
+                                    )
+                                elif schema_type in ["view"]:
+                                    response = jsonify(full_schema.get("view", {}))
+                                del inner_task
+                                return response
 
-                            log.error(
-                                f"unable to register {task_dir}: {e} {traceback.format_exc()}"
+                            @api.doc(
+                                f"create_task_instance_for_{task_info['path'].replace(os.path.sep,'_')}"
                             )
+                            @api.expect(
+                                task_models[task_info["path"]], validate=False
+                            )
+                            def post(self):
+                                """
+                                Creates an instance of the task and returns the job_id
+                                """
+
+                                from jinjamator.task.celery import (
+                                    run_jinjamator_task,
+                                )
+                                from jinjamator.daemon.database import db
+
+                                relative_task_path = request.endpoint.replace(
+                                    "api.", ""
+                                )
+                                data = request.get_json()
+                                job_id = str(uuid.uuid4())
+                                job = run_jinjamator_task.apply_async(
+                                    [
+                                        relative_task_path,
+                                        data,
+                                        data.get("output_plugin", "console"),
+                                    ],
+                                    task_id=job_id,
+                                )
+
+                                db_job = list(
+                                    db.session.query(DB_Job).filter(
+                                        DB_Job.task_id == job.id
+                                    )
+                                )
+                                db_job = db_job and db_job[0]
+                                if not db_job:
+                                    db_job = DB_Job(job.id)
+                                    db_job.status = "SCHEDULED"
+                                    db_job.configuration = data
+                                    db_job.jinjamator_task = relative_task_path
+                                    db.session.add(db_job)
+                                    db.session.flush()
+                                    db.session.commit()
+
+                                return jsonify({"job_id": job.id})
+
+                    except Exception as e:
+                        import traceback
+
+                        log.error(
+                            f"unable to register {task_dir}: {e} {traceback.format_exc()}"
+                        )
 
 
 @ns.route("/")
