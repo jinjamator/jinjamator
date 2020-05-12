68aa85bd5254d4d083d1a96f14cb2d9bdc759961
Wilhelm Putz <jinjamator@aci.guru>
2020-05-12 08:36:37 +0200

    add login route for now

--- jinjamator/daemon/webui/__init__.py
+++ jinjamator/daemon/webui/__init__.py
@@ -7,6 +7,7 @@ webui = Blueprint("webui", __name__, url_prefix="/")
 log = logging.getLogger()
 
 
+@webui.route("/index.html", methods=["GET"])
 @webui.route("/", methods=["GET"])
 def index():
     """
@@ -14,4 +15,14 @@ def index():
     """
     return send_from_directory(
         os.path.sep.join([os.path.dirname(__file__), "static"]), "index.html"
+    )
+
+
+@webui.route("/login.html", methods=["GET"])
+def login():
+    """
+    Just send static login.html
+    """
+    return send_from_directory(
+        os.path.sep.join([os.path.dirname(__file__), "static"]), "login.html"
     )
