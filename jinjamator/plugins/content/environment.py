d57018ecc3b5ecf112677ddb42529d2e368c9c70
Wilhelm Putz <jinjamator@aci.guru>
2020-05-21 21:14:11 +0200

    change namespace

--- jinjamator/plugins/content/environment.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-log = logging.getLogger()
-
-
-def site_path():
-    return self._parent._configuration.get("jinjamator_site_path", None)
---
+++ jinjamator/plugins/content/jm/environment.py
@@ -0,0 +1,19 @@
+import logging
+from jinjamator.plugins.content.file import load
+import os
+
+log = logging.getLogger()
+
+
+def site_path():
+    return self._parent._configuration.get("jinjamator_site_path", None)
+
+
+def jinjamator_base_directory():
+    return self._parent._configuration.get("jinjamator_base_directory", None)
+
+
+def python_requirements():
+    return load(os.path.join(jinjamator_base_directory(), "requirements.txt")).split(
+        "\n"
+    )
