49358576118a44004ea9e774ce514bd845ff33a1
Wilhelm Putz <jinjamator@aci.guru>
2020-05-21 21:19:43 +0200

    use new return format

--- tests/plugins/content/task/01_run.py
+++ tests/plugins/content/task/01_run.py
@@ -1,8 +1,8 @@
 ok = True
-for subtask_path, return_value in task.run(".subtask1/", output_plugin="null").items():
-    if "01_tasklet_1" not in subtask_path:
+for retval in task.run(".subtask1/", output_plugin="null"):
+    if "01_tasklet_1" not in retval["tasklet_path"]:
         ok = False
-    if return_value != "OK":
+    if retval["result"] != "OK":
         ok = False
 
 if ok:
