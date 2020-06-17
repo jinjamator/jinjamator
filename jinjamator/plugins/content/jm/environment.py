b11771554e85c403b0338ab4f72e83759524fcec
Wilhelm Putz <jinjamator@aci.guru>
2020-06-17 22:46:30 +0200

    add task_base_dir helper

--- jinjamator/plugins/content/jm/environment.py
+++ jinjamator/plugins/content/jm/environment.py
@@ -13,6 +13,10 @@ def jinjamator_base_directory():
     return _jinjamator._configuration.get("jinjamator_base_directory", None)
 
 
+def jinjamator_global_tasks_base_dirs():
+    return _jinjamator._configuration.get("global_tasks_base_dirs", None)
+
+
 def python_requirements():
     return load(os.path.join(jinjamator_base_directory(), "requirements.txt")).split(
         "\n"
