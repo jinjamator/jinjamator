7cea16f6f8e2e6f1baed924173f77a9375acc43f
Wilhelm Putz <jinjamator@aci.guru>
2020-06-19 08:50:07 +0200

    make output more fancy

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -18,6 +18,10 @@ logger.handlers.pop()
 logger.addHandler(fh)
 logger.setLevel(logging.DEBUG)
 
+print(f"\n-------------------------------------------------------------------")
+print(f"Setting up testbed")
+print(f"-------------------------------------------------------------------\n")
+
 print(f"Logfile for this run is {log_file}")
 
 # logger.disabled = True
@@ -28,33 +32,44 @@ test_paths = []
 cfg = self.configuration
 
 if "plugins" in run_tests or "all" in run_tests:
-    print("adding plugin tests to test list")
-    test_paths += glob(
+    print("adding plugin tests to test list", end=" ")
+
+    tmp = glob(
         f"{os.path.dirname(__file__)}{os.path.sep}plugins/{os.path.sep}**{os.path.sep}*.py",
         recursive=True,
     ) + glob(
         f"{os.path.dirname(__file__)}{os.path.sep}plugins/{os.path.sep}**{os.path.sep}*.j2",
         recursive=True,
     )
+    test_paths += tmp
+
+    print(Fore.GREEN + "DONE " + Fore.WHITE + str(len(tmp)) + Style.RESET_ALL)
 
 
 if "api" in run_tests or "all" in run_tests:
-    print("adding api tests to test list")
-    test_paths += glob(
+    print("adding api tests to test list", end=" ")
+
+    tmp = glob(
         f"{os.path.dirname(__file__)}{os.path.sep}api/{os.path.sep}**{os.path.sep}*.py",
         recursive=True,
     )
-    print("creating test container for api tests")
+    test_paths += tests
+    print(Fore.GREEN + "DONE " + Fore.WHITE + str(len(tmp)) + Style.RESET_ALL)
+
+    print("creating test container for api tests", end=" ")
     container_data = task.run("helper/create_docker_container", output_plugin="null")[
         1
     ]["result"]
     cfg["_container"] = container_data
-    log.info("done creating test container for api tests")
+    print(Fore.GREEN + "DONE" + Style.RESET_ALL)
 
 
 if "tasks" in run_tests or "all" in run_tests:
-    print("adding task unit tests to test list")
-    test_paths += task.run("helper/get_task_tests", output_plugin="null")[0]["result"]
+    print("adding task unit tests to test list", end=" ")
+
+    tmp = task.run("helper/get_task_tests", output_plugin="null")[0]["result"]
+    test_paths += tests
+    print(Fore.GREEN + "DONE " + Fore.WHITE + str(len(tmp)) + Style.RESET_ALL)
 
 
 for tasklet_path in test_paths:
@@ -62,10 +77,14 @@ for tasklet_path in test_paths:
     if task_path not in tests:
         tests.append(task_path)
 failed = 0
+skipped = 0
 
+print(f"\n\n-------------------------------------------------------------------")
+print(f"Starting Test run")
+print(f"-------------------------------------------------------------------\n")
 
 for test in tests:
-    print(f"running test {test}:")
+    print(f"running tests defined in {test}:")
     try:
         for retval in task.run(test, cfg, output_plugin="null"):
             tasklet_path = retval["tasklet_path"]
@@ -87,6 +106,7 @@ for test in tests:
                     failed += 1
                     for skipped_task in tasklet_skipped:
                         print("\t" + skipped_task + Fore.YELLOW + " SKIPPED")
+                        skipped += 1
 
                 print(Style.RESET_ALL, end="")
             elif "/output/" in tasklet_path:
@@ -95,7 +115,6 @@ for test in tests:
             else:
                 print(Fore.YELLOW + "NOT IMPLEMENTED")
                 print(Style.RESET_ALL, end="")
-
     except Exception as e:
         print(f"\t{os.path.basename(tasklet_path)}", end=" ")
         print(Fore.RED + "NOT OK")
@@ -104,10 +123,15 @@ for test in tests:
 
         failed += 1
         print(Style.RESET_ALL, end="")
+    print(f"")
 
 
 print(Style.RESET_ALL, end="")
+print(f"\n\n-------------------------------------------------------------------")
+print(f"Test run summary")
+print(f"-------------------------------------------------------------------\n")
 print(f"Failed tests: {failed}")
-print(f"Logfile for this run is {log_file}")
+print(f"Skipped tests: {skipped}")
+print(f"\nLogfile for this run is {log_file}")
 if failed:
     sys.exit(1)
