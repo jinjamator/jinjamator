90d26f0e29e31eee04fa695222b359a61e593447
Berger Philipp <BERGERP@kapsch.net>
2020-06-23 10:10:49 +0200

    added test-filter
    added filter-string to filter tests

--- tests/helper/get_task_tests/01_get_all_tasks_with_tests.py
+++ tests/helper/get_task_tests/01_get_all_tasks_with_tests.py
@@ -4,8 +4,14 @@ import os
 retval = []
 for base_dir in jm.environment.jinjamator_global_tasks_base_dirs():
     for test in glob(os.path.join(base_dir, "**", ".tests", "*.py"), recursive=True):
-        retval.append(test)
+        if len(task_string) > 0:
+            if task_string in test: retval.append(test)
+            else: log.info("Skipped test due to task-string filter: "+test)
+        else: retval.append(test)
     for test in glob(os.path.join(base_dir, "**", ".tests", "*.j2"), recursive=True):
-        retval.append(test)
+        if len(task_string) > 0:
+            if task_string in test: retval.append(test)
+            else: log.info("Skipped test due to task-string filter: "+test)
+        else: retval.append(test)
 
 return retval
