fdb92bc05b19e394925bb14f936240553d3a5bf1
Wilhelm Putz <jinjamator@aci.guru>
2020-09-16 14:29:20 +0200

    set X-Frame-Options DENY

--- jinjamator/daemon/app.py
+++ jinjamator/daemon/app.py
@@ -1,3 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__, static_folder="./webui/static")
+
+
+@app.after_request
+def apply_header_security(response):
+    response.headers["X-Frame-Options"] = "DENY"
+
+    return response
