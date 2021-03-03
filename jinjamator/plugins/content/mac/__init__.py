46041fae5eae5586022df07e2fdd5ea1c9527307
Wilhelm Putz <jinjamator@aci.guru>
2021-03-03 14:58:27 +0100

    fix paths

--- jinjamator/plugins/content/mac/__init__.py
+++ jinjamator/plugins/content/mac/__init__.py
@@ -1,9 +1,11 @@
-from netaddr import EUI
+from netaddr import EUI, mac_unix_expanded
+
 
 def to_unix(mac_address):
     mac = EUI(mac_address)
     mac.dialect = mac_unix_expanded
     return str(mac)
+
 
 def to_aci(mac_address):
     return to_unix(mac_address)
