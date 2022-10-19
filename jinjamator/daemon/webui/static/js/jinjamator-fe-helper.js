559a3e0873767a3cfb84466d26ffe3f18602285a
Wilhelm Putz <jinjamator@aci.guru>
2022-10-19 14:13:26 +0200

    fe helper, multiple extensions and fixes

--- jinjamator/daemon/webui/static/js/jinjamator-fe-helper.js
+++ jinjamator/daemon/webui/static/js/jinjamator-fe-helper.js
@@ -5,23 +5,44 @@ var result_cache = {}
 
 log.setLevel(logging.FE_DEBUG)
 
-function table_dropdown_set_value(select, container) {
-    window.setTimeout( // this is needed for alpaca to have data in the parent row
-        function () {
+function user_error(message,button_on_click_callback,button_text="Abort"){
+    $('#modal-error').find('.modal-message')[0].innerText=message
+    $('#modal-error').modal('show')
+}
 
+
+function table_dropdown_set_value(select, container, options) {
+    let local_options= options
+    Alpaca.nextTick( // this is needed for alpaca to have data in the parent row
+        function () {
+            let options=local_options
             let dropdown = Alpaca.fieldInstances[select[0].id]
-            let options = dropdown.options.jinjamator.table_dropdown_set_value
+            
+            
+            if (!options){
+                options = dropdown.options.jinjamator.table_dropdown_set_value
+            }
+
             $(container[0]).children("button")[0].disabled = true
             $(container[0]).children("button")[0].title = 'Loading...'
             $(container[0]).children("button").children("span")[0].innerText = 'Loading...'
-
+            
             var site = $('#jinjamator_environment option:selected').val()
-            let url = options.url
+            // log.debug("table_dropdown_set_value","options",options)
+            if (options.url ){
+                var url = options.url
+            }
+            else{
+                log.error("table_dropdown_set_value",dropdown.path,"url not set in options",options)
+                return -1
+            }
+            
+
             let mappings = options.mappings
             let post_data = options.post_data
-
+            
             let task_configuration = process_mappings(post_data, dropdown.parent.getValue(), dropdown)
-            log.debug(select[0].id, "task_configuration after mapping", task_configuration)
+            log.debug("table_dropdown_set_value",dropdown.path, "task_configuration after mapping", task_configuration)
             client.tasks.read(url,
                 {
                     'preload-data': JSON.stringify(task_configuration),
@@ -67,7 +88,7 @@ function table_dropdown_set_value(select, container) {
                 })
 
         }
-        , 1)
+        )
 
 
 }
@@ -82,7 +103,7 @@ function process_mappings(mappings, data, obj = {}) {
             try {
                 data[key] = jq.json(data, code)
             } catch (e) {
-                log.error(e)
+                log.error("process_mappings",e,"code",code,"key",key,"mappings",mappings,"data",data,"obj",obj)
             }
         }
 
@@ -92,11 +113,16 @@ function process_mappings(mappings, data, obj = {}) {
 
 }
 
-function dropdown_fill_dropdown(element, checked) {
+function dropdown_fill_dropdown(element, checked, options=false) {
     let dropdown_id = element[0].parentNode.id
-    let options = Alpaca.fieldInstances[dropdown_id].options.jinjamator.dropdown_fill_dropdown
+    if (options === "undefined"){
+        var options = Alpaca.fieldInstances[dropdown_id].options.jinjamator.dropdown_fill_dropdown
+    }
     dropdown_fill_callback(element, checked, options, function (url, target, task_result) {
-        log.setPrefix("dropdown_fill_dropdown/dropdown_fill_callback/callback")
+        let dropdown_id = element[0].parentNode.id
+        let dropdown = Alpaca.fieldInstances[dropdown_id]
+
+        log.debug("dropdown_fill_dropdown/dropdown_fill_callback/callback",dropdown.path, "task_result",task_result)
         for (const [key, value] of Object.entries(task_result)) {
             target.schema.enum.push(key);
             target.options.optionLabels.push(value["label"]);
@@ -108,20 +134,22 @@ function dropdown_fill_dropdown(element, checked) {
     })
 }
 
-function dropdown_fill_table(element, checked) {
+function dropdown_fill_table(element, checked, options) {
     let dropdown_id = element[0].parentNode.id
-    let options = Alpaca.fieldInstances[dropdown_id].options.jinjamator.dropdown_fill_table
+    if (!options){
+        var options = Alpaca.fieldInstances[dropdown_id].options.jinjamator.dropdown_fill_table
+    }
     dropdown_fill_callback(element, checked, options, function (url, target, task_result) {
-        log.setPrefix("dropdown_fill_table/dropdown_fill_callback/callback")
         let dropdown_id = element[0].parentNode.id
         let dropdown = Alpaca.fieldInstances[dropdown_id]
         let element_value = element[0].value
-
+        log.debug("dropdown_fill_table/dropdown_fill_callback/callback",dropdown.path,"->",target.path, "element_value",element_value)
 
         let row_data = {}
         if (options.mappings !== undefined) {
             $("#overlay").fadeIn(100); // this is needed because emscripten jq is noticeably slow
             task_result[element_value] = process_mappings(options.mappings, task_result[element_value]);
+            log.debug("dropdown_fill_table/dropdown_fill_callback/callback",dropdown.path,"->",target.path, "task_result[element_value]",task_result[element_value])
             window.setTimeout(
                 function () { $("#overlay").fadeOut(1); }
                 , 150)
@@ -133,7 +161,7 @@ function dropdown_fill_table(element, checked) {
             try {
                 row_data[key] = task_result[element_value][key]
             } catch (e) {
-                log.error(e)
+                log.error("dropdown_fill_table/dropdown_fill_callback/callback",dropdown.path,"->",target.path,e)
                 row_data[key] = ""
             }
 
@@ -163,7 +191,7 @@ function dropdown_fill_table(element, checked) {
 
 function dropdown_fill_callback(element, checked, options, callback) {
     // helper function to update a target alpaca item based on the results of a backend jinjamator task
-    if (options.collapse_on_select){
+    if (options && options.collapse_on_select){
         $(document).click(); // collapse dropdown
     }
     
@@ -174,18 +202,24 @@ function dropdown_fill_callback(element, checked, options, callback) {
     let value = element[0].value
     let site = $('#jinjamator_environment option:selected').val()
     let task_configuration = {}
+    let is_multiple = false
+
+    if (Alpaca.fieldInstances[dropdown_id].options.multiple){
+        is_multiple=Alpaca.fieldInstances[dropdown_id].options.multiple
+    }
 
 
     try {
         var target_name = options.target
     } catch (e) {
-        log.error("mandatory config.", dropdown.name, ".options.jinjamator.target is missing")
+        log.debug(options)
+        log.error(dropdown.path,"mandatory config.", dropdown.name, ".options.jinjamator.target is missing")
         throw (e)
     }
     try {
         var url = options.url
     } catch (e) {
-        log.error("", dropdown.name, ".options.jinjamator.url undefined")
+        log.error(dropdown.path, dropdown.name, ".options.jinjamator.url undefined")
         throw (e)
     }
 
@@ -193,7 +227,7 @@ function dropdown_fill_callback(element, checked, options, callback) {
         var target = $("#form").alpaca("get").getControlByPath(target_name);
     }
     catch (e) {
-        log.error("cannot find alpaca target field.", target_name)
+        log.error(dropdown.path,"cannot find alpaca target field.", target_name)
         throw (e)
     }
     
@@ -210,18 +244,22 @@ function dropdown_fill_callback(element, checked, options, callback) {
 
     if (url.startsWith("cache:")) {
         cache_key = url.substring(6)
-        log.debug("cache URL", cache_key)
+        log.debug(dropdown.path,"cache URL", cache_key)
         task_result = result_cache[cache_key]
-        callback(url, target, task_result);
-        return 0
+        if (task_result){
+            log.debug(dropdown.path,"cache hit for key",cache_key,result_cache[cache_key])
+            callback(url, target, task_result);
+            return 0    
+        }
+        else{
+            log.debug(dropdown.path,"cache miss for key",cache_key,"doing lookup")
+        }
     }
-
 
     if (!(url in result_cache)) {
         result_cache[url] = {};
         result_cache[url]['byElement'] = {};
     }
-
 
     if (checked == true) {
 
@@ -232,29 +270,33 @@ function dropdown_fill_callback(element, checked, options, callback) {
                 'preload-defaults-from-site': site
             }).done(function (preload_result) {
                 task_configuration = preload_result['data']
-                log.debug('got task configuration', task_configuration)
+                log.debug("dropdown_fill_callback",dropdown.path,'got task configuration', task_configuration)
                 task_configuration['output_plugin'] = "json";
                 client.tasks.create(url, task_configuration).done(function (task_result) {
-                    log.debug('got task result', task_result)
+                    log.debug("dropdown_fill_callback",dropdown.path,'got task result', task_result)
+                    if (task_result.error){
+                        user_error(task_result.error)
+                        return -1
+                    }
                     callback(url, target, task_result);
                     target.refresh(function () {
                         $("#overlay").fadeOut(1);
                     });
 
                 }).fail(function (e) {
-                    log.error("failed to create task", "url:", url, "task_configuration", task_configuration, 'site', site)
+                    log.error(dropdown.path,"failed to create task", "url:", url, "task_configuration", task_configuration, 'site', site)
                     $("#overlay").fadeOut(1);
                     throw (e)
                 })
 
             }).fail(function (e) {
-                log.error("cannot preload task data ", "url:", url, "task_configuration", task_configuration, 'site', site)
+                log.error(dropdown.path,"cannot preload task data ", "url:", url, "task_configuration", task_configuration, 'site', site)
                 $("#overlay").fadeOut(1);
                 throw (e)
             })
 
     } else {
-        log.debug("deselecting values", result_cache[url]['byElement'][element])
+        log.debug(dropdown.path,"deselecting values", result_cache[url]['byElement'][element])
         // $("#overlay").fadeIn(100);
         for (const [key, value] of Object.entries(result_cache[url]['byElement'][element])) {
             const index = target.schema.enum.indexOf(key);
@@ -274,5 +316,129 @@ function dropdown_fill_callback(element, checked, options, callback) {
         target.refresh(function () {
             log.debug("finished deselecting")
         });
+    }
+}
+
+function dropdown_set_value(element, checked, options, callback) {
+    // helper function to update a target alpaca item based on the results of a backend jinjamator task
+    if (options && options.collapse_on_select){
+        $(document).click(); // collapse dropdown
+    }
+    
+
+    let element_index = element[0].index
+    let dropdown_id = element[0].parentNode.id
+    let dropdown = Alpaca.fieldInstances[dropdown_id]
+    let value = element[0].value
+    let site = $('#jinjamator_environment option:selected').val()
+    let task_configuration = {}
+
+
+    try {
+        var target_name = options.target
+    } catch (e) {
+        log.debug(options)
+        log.error(dropdown.path,"mandatory config.", dropdown.name, ".options.jinjamator.target is missing")
+        throw (e)
+    }
+    try {
+        var url = options.url
+    } catch (e) {
+        log.error(dropdown.path, dropdown.name, ".options.jinjamator.url undefined")
+        throw (e)
+    }
+
+    try {
+        var target = $("#form").alpaca("get").getControlByPath(target_name);
+    }
+    catch (e) {
+        log.error(dropdown.path,"cannot find alpaca target field.", target_name)
+        throw (e)
+    }
+    
+    // log.debug(dropdown.path, " index: ", element_index, " ", element, " is checked", checked, " value is: ", value)
+
+    // convert get parameter to post
+    if (url.includes("?")) {
+        [url, params] = url.replace('{value}', value).split("?")
+        for (const param of new URLSearchParams("?" + params).entries()) {
+            task_configuration[param[0]] = param[1]
+        }
+        log.debug("URL", url)
+    }
+
+    if (url.startsWith("cache:")) {
+        cache_key = url.substring(6)
+        log.debug("dropdown_set_value",dropdown.path,"cache URL", cache_key)
+        task_result = result_cache[cache_key]
+        if (task_result){
+            log.debug("dropdown_set_value",dropdown.path,"cache hit for key",cache_key,result_cache[cache_key])
+            callback(url, target, task_result);
+            return 0    
+        }
+        else{
+            log.debug("dropdown_set_value",dropdown.path,"cache miss for key",cache_key,"doing lookup")
+        }
+    }
+
+
+    if (!(url in result_cache)) {
+        result_cache[url] = {};
+        result_cache[url]['byElement'] = {};
+    }
+
+
+    if (checked == true) {
+
+        client.tasks.read(url,
+            {
+                'preload-data': JSON.stringify(task_configuration),
+                'preload-defaults-from-site': site
+            }).done(function (preload_result) {
+                task_configuration = preload_result['data']
+                log.debug("dropdown_set_value",dropdown.path,'got task configuration', task_configuration)
+                task_configuration['output_plugin'] = "json";
+                client.tasks.create(url, task_configuration).done(function (task_result) {
+                    log.debug("dropdown_set_value",dropdown.path,'got task result', task_result)
+                    if (task_result.error){
+                        user_error(task_result.error)
+                        return -1
+                    }
+
+                    let mappings = options.mappings
+                    task_result = process_mappings(mappings, task_result);
+                    target.setValue(task_result[target.propertyId])
+                    // force trigger onChange of multiselect
+                    $('#' + target.id).multiselect('select', task_result[target.propertyId],true);
+                    log.debug("dropdown_set_value",dropdown.path,"target",target)    
+
+                }).fail(function (e) {
+                    log.error("dropdown_set_value",dropdown.path,"failed to create task", "url:", url, "task_configuration", task_configuration, 'site', site)
+                    throw (e)
+                })
+
+            }).fail(function (e) {
+                log.error("dropdown_set_value",dropdown.path,"cannot preload task data ", "url:", url, "task_configuration", task_configuration, 'site', site)
+                throw (e)
+            })
+
+    } else {
+        log.debug(dropdown.path,"deselecting values", result_cache[url]['byElement'][element])
+        // for (const [key, value] of Object.entries(result_cache[url]['byElement'][element])) {
+        //     const index = target.schema.enum.indexOf(key);
+
+        //     if (index > -1) {
+        //         target.schema.enum.splice(index, 1);
+        //         target.options.optionLabels.splice(index, 1);
+        //         for (const [d_i, d_v] of Object.entries(target.data)) {
+        //             if (key == d_v.text) {
+        //                 target.data.splice(d_i, 1);
+        //             }
+        //         }
+
+        //     }
+
+        // };
+ 
     }
 }
