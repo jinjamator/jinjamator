1e829a85ba3c366e1182f2b89d100f9317d28626
Nefti <65944159+Nefti-sama@users.noreply.github.com>
2022-11-18 16:47:07 +0100

    added list2dict and get_by_value to list_tools

--- jinjamator/tools/list_tools.py
+++ jinjamator/tools/list_tools.py
@@ -36,3 +36,76 @@ def dict_wo (haystack,needles):
     elif not isinstance(needles,list): return False
 
     return {k: haystack[k] for k in haystack.keys() - needles}
+
+
+def list2dict (list_orig,key_name,**kwargs):
+    """
+    Converts a list of dictionaries into a dictionary using the specified ``key_name`` as key
+
+
+    :param list_orig: The original list
+    :type list_orig: ``list``
+    :param key_name: The name on the dictionary-item that should be used as a key
+    :type key_name: ``str``
+    :Keyword Arguments:
+    * *duplicates* (``str``) --
+        Tells the function how to deal with duplicates.
+        Could be:
+            - overwrite: Overwrites duplcate keys (default)
+            - skip: Skips duplicate keys and just uses the first one
+            - add: Creates the element and appends the index of the list
+
+    :return: Dictionary with all list-elements using ``key_name`` as key
+    :rtype: ``dict``
+    """
+    dodup = "overwrite"
+    if "duplicates" in kwargs:
+        if kwargs['duplicates'] == 'overwrite': dodup = "overwrite"
+        elif kwargs['duplicates'] == 'skip': dodup = "skip"
+        elif kwargs['duplicates'] == 'add': dodup = "add"
+    
+    ret = {}
+    i = 1
+    for elem in list_orig:
+        if isinstance(elem,dict) or isinstance(elem,list):
+            if key_name in elem:
+                key = elem[key_name]
+                
+                if key in ret:
+                    #Handle duplicates
+                    if dodup == "skip":
+                        continue
+                    elif dodup == "add":
+                        key = f"{key}_dup_{str(i)}"
+                
+                ret[key] = elem
+        
+        i += 1
+    
+    return ret
+
+
+def get_by_value (key_name,value,iterable):
+    """
+    Returns the item of a list of dictionaries or a dictionary containing dictionaries, where the ``key`` has the value ``value``
+
+    :param key_name: The name of the key
+    :type key_name: ``str``
+    :param value: The value the specified key has
+    :type value: ``str``
+    :param iterable: The dict/list in which the lookup should be done
+    :type iterable: ``list``|``dict``
+    :return: Dictionary where ``key`` has the value ``value``. Returns False of nothing was found
+    :rtype: ``dict`` | ``bool``
+    """
+    if isinstance(iterable,list):
+        for e in iterable:
+            if key_name in e and e[key_name] == value:
+                return e
+    elif isinstance(iterable,dict):
+        for e in iterable.values():
+            if key_name in e and e[key_name] == value:
+                return e
+    
+    return False
+    
