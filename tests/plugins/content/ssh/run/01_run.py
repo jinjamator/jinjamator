e222c50a647f809d8b5658b17f4ce83572ea2841
putzw <wilhelm.putz@kapsch.net>
2023-04-20 17:06:04 +0200

    this should be handled by injection

--- tests/plugins/content/ssh/run/01_run.py
+++ tests/plugins/content/ssh/run/01_run.py
@@ -11,6 +11,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-ssh_username, ssh_password, ssh_host
 data = ssh.run("show run")
 return "OK"
