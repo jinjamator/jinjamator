c36c520fdb5c2b3b0c2c4a34f2f7f764d586f8ca
putzw <wilhelm.putz@kapsch.net>
2023-04-22 10:11:27 +0200

    fix linux

--- jinjamator/plugins/content/linux/__init__.py
+++ jinjamator/plugins/content/linux/__init__.py
@@ -3,13 +3,6 @@ import subprocess
 import re
 import pkgutil
 
-# __all__ = []
-# pprint(__builtins__)
-# for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
-#     __all__.append(module_name)
-#     _module = loader.find_module(module_name).load_module(module_name)
-#     # setattr(__builtins__[__package__],module_name,_module)
-
 import linux.file
 import linux.disk
 import linux.systemctl
