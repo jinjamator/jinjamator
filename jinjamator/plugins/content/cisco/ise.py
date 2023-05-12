aea7538ed31ff45a2fc86b05bf4ada5751c00da5
putzw <wilhelm.putz@k-business.com>
2023-05-12 10:52:02 +0200

    add minimal ise plugin to public source

---
+++ jinjamator/plugins/content/cisco/ise.py
@@ -0,0 +1,16 @@
+# this is just a helper plugin to make pyise-ers module available as plugin
+
+def _get_missing_ise_connection_vars():
+    inject = []
+    for required_var in ['ise_hostname', 'ise_username', 'ise_password' ]:
+        if required_var not in _jinjamator.configuration._data:
+            inject.append(required_var)
+    return inject
+    
+def connect(verify=False,disable_warnings=True, *,_requires=_get_missing_ise_connection_vars):
+    try:
+        from pyiseers import ERS
+    except:
+        log.error('module pyiseers not available install via pip/pipx install pyise-ers')
+        return None
+    return ERS(ise_node=_jinjamator.configuration._data['ise_hostname'], ers_user=_jinjamator.configuration._data['ise_username'], ers_pass=_jinjamator.configuration._data['ise_password'], verify=verify, disable_warnings=disable_warnings)
