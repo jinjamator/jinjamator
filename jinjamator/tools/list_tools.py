d6c5b7227a9d71ee6cfb08c60714cc7c174d1765
Nefti <65944159+Nefti-sama@users.noreply.github.com>
2024-07-04 15:36:12 +0200

    New list_tools function
    + get_by_key_regex (key_re,iterable,ignorecase=False)
    + try_to_fill (base,values)
    + update_empty_values (first,second,create_keys=True)

--- jinjamator/tools/list_tools.py
+++ jinjamator/tools/list_tools.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+import re
 
 
 def list_wo (haystack,needles):
@@ -108,4 +109,79 @@ def get_by_value (key_name,value,iterable):
                 return e
     
     return False
+
+
+def get_by_key_regex (key_re,iterable,ignorecase=False):
+    """
+    Returns a dict of items where the key matches the regex given
+
+    :param key_re: The regex to look for
+    :type key_re: ``str``
+    :param iterable: The dict in which the lookup should be done
+    :type iterable: ``dict``
+    :Keyword Arguments:
+    * *ignorecase* (``bool``) --
+        Ignore case sensivity in regex.
+        False by default
+    :return: Dictionary where the keys match the regex. Returns empty dict if nothing matched
+    :rtype: ``dict``
+    """
+    if ignorecase == False: reg = re.compile(key_re)
+    else: reg = re.compile(key_re,re.IGNORECASE)
+
+    ret = {}
+    for k,v in iterable.items():
+        if reg.match(k): ret[k] = v
     
+    return ret
+
+
+def try_to_fill (base,values):
+    """
+    Tries to fill the ``base`` dict with values from ``values``
+    Will iterate over ``base`` looking for keys in ``values`` - if the key exist, overwrite base key with values key
+
+    :param base: The base dict
+    :type base: ``dict``
+    :param values: The values dict
+    :type values: ``dict``
+    :return: ``base`` dictionary with values of ``values`` dict if key existed
+    :rtype: ``dict``
+    """
+    iter_list = base.keys()
+    for idx in iter_list:
+        try: base[idx] = values[idx]
+        except: pass
+    
+    return base
+
+
+def update_empty_values (first,second,create_keys=True):
+    """
+    Will update empty values of keys in ``first`` dict with the values of ``second`` dict
+    Empty is an empty string ("") or ``None``
+    If ``create_keys`` is set to ``True``, keys that exist in ``second`` but not in ``first`` will be created
+
+    :param first: First dict
+    :type first: ``dict``
+    :param second: Second dict
+    :type second: ``dict``
+    :param create_keys: Create keys if they are present in ``second`` buit not in ``first``. Default: True
+    :type create_keys: ``boolean``
+    :return: ``dict`` whith updated values
+    :rtype: ``dict``
+    """
+    #If we create new keys instead of only updating existing ones, merge the dicts, preserving the values of first
+    if create_keys == True: dict_res = second | first
+    else: dict_res = first
+    
+    #Check each key of second and update the value if
+    # - key exists
+    # - and the value is an empty string or none
+    for k,v in second.items():
+        if k in dict_res.keys():
+            #An empty string or None counts as empty
+            if dict_res[k] == "" or dict_res[k] == None:
+                dict_res[k] = v
+    
+    return dict_res
