170ef7ea91016abc1593e728529ce803a3fe40d0
Wilhelm Putz <wilhelm.putz@cancom.com>
2026-01-29 23:59:00 +0100

    if someone wants to know the log dir for the job, create it implicitly

--- jinjamator/plugins/content/jm/__init__.py
+++ jinjamator/plugins/content/jm/__init__.py
@@ -1,8 +1,10 @@
 import os
 
 def get_job_log_files_dir():
-    return os.path.join(_jinjamator._configuration.get("jinjamator_user_directory"),"logs", _jinjamator._configuration.get("jinjamator_job_id"),"files")
-
+    path=os.path.join(_jinjamator._configuration.get("jinjamator_user_directory"),"logs", _jinjamator._configuration.get("jinjamator_job_id",str(id(_jinjamator))),"files")
+    os.makedirs(path,exist_ok=True)
+    return path 
+    
 def run_output_plugin(data):
     _jinjamator._output_plugin.connect()
     try:
